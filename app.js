289f769623a99ec099ef3deb5c39a2fc3d0a5269
Kolawole Taiwo <taiking005.dev@gmail.com>
2022-08-05 11:08:39 +0100

    update button

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ let adviceData = null;
 const url = "https://api.adviceslip.com/advice";
 
 async function dice() {
+    buttonDice.innerHTML = `<i class="fa-solid fa-sync fa-spin"></i>`
     fetch(url)
         .then(response => response.json())
         .then(data => {
             adiveId.textContent = data.slip.id;
             advice.textContent = data.slip.advice;
-            // console.log(data.slip)
+            buttonDice.innerHTML = `<img src="./images/icon-dice.svg" alt="dice" /> `
         })
 }
 
